4b0174ef780ad4a92bf195473ba25f83751800a3
Dyesse YUMBA <yumba.nduba@gmail.com>
2020-05-12 18:07:38 +0100

    ✨ Add SCH/STH dataset table

---
+++ src/sch_sth/impact_assessment/dashboard/23_urine_table.sql
@@ -0,0 +1,15 @@
+/*
+ * File: 23_urine_table.sql
+ * File Created: Tuesday, 12th May 2020 6:06:32 pm
+ * Author: Dyesse YUMBA
+ * Last Modified: Tuesday, 12th May 2020 6:06:55 pm
+ * Modified By: Dyesse YUMBA
+ * -----
+ * (c) 2020, WHO/AFRO/UCN/ESPEN
+ */
+
+
+/*
+ * Variable to rename <%v_ab_cde_fgh_4_urine%>
+ */
+ SELECT * FROM <%v_ab_cde_fgh_4_urine%>
